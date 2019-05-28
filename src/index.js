cb03aa179ebc9f6ecf5efcaff3643a7f17b998e3
Kevin Taulman <kevin.taulman@icloud.com>
2019-05-28 06:50:35 -0700

    updated with a map loop in the CardList file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //import cards 
-import Card from "./Card";
 import {robots} from './robots';//use {robots} not robots b/c of multiple key:value pairs.
+import CardList from './CardList';
 
 //formatting import
 import 'tachyons';
@@ -12,12 +12,7 @@ import * as serviceWorker from './serviceWorker';
 
 //What Appears on the Webpage
 ReactDOM.render(
-    
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <CardList robots={robots}/>
 , document.getElementById('root')
 );
 
