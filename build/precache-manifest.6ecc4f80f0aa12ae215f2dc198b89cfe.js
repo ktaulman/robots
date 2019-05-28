aa54878a8d44ceeb57a7c7b7a2b4ec7caaabf6a5
Kevin Taulman <kevin.taulman@icloud.com>
2019-05-28 09:48:58 -0700

    finalized

---
+++ build/precache-manifest.6ecc4f80f0aa12ae215f2dc198b89cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c0212061cf2a6c5ac52653317414fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "497fd8f4f6b4c8ae5672",
+    "url": "/static/css/main.7d8986d6.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "497fd8f4f6b4c8ae5672",
+    "url": "/static/js/main.37244a94.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
+import CardList from '../components/CardList';
 //import {robots} from './robots';
-import SearchBox from './SearchBox';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 //PROPS== what you pass in. Come out of STATE 
 //STATE== object that describes your application.
@@ -27,20 +28,22 @@ class App extends Component{
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots=>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots,searchfield}=this.state;
+        const filteredRobots = robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length ===0){
-           return  <h1>Loading</h1>
-        } else{
-        return(
+        return !robots.length?
+        <h1>Loading</h1>:
+        (
             <div className="tc">
                 <h1 id="robofriends">RoboFriends </h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll> 
             </div>
         );
-    }
     }
 }
 
